8976cce13d7340af6e0b499780deae51ec42672e
Pierre-Cognard <pierre.cognard@alumni.univ-avignon.fr>
2023-04-20 10:13:32 +0200

    PokedexTest

---
+++ src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
@@ -0,0 +1,71 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class PokedexTest {
+
+    private IPokemonMetadataProvider metadataProvider;
+    private IPokemonFactory pokemonFactory;
+    private Pokedex pokedex;
+
+    @BeforeEach
+    public void setUp() {
+        metadataProvider = mock(IPokemonMetadataProvider.class);
+        pokemonFactory = mock(IPokemonFactory.class);
+        pokedex = new Pokedex(metadataProvider, pokemonFactory);
+    }
+
+    @Test
+    public void getPokemonMetadataTest() throws PokedexException {
+        PokemonMetadata metadata = new PokemonMetadata(1, "Bulbasaur", 126, 126, 90);
+        when(metadataProvider.getPokemonMetadata(1)).thenReturn(metadata);
+        assertEquals(metadata, pokedex.getPokemonMetadata(1));
+    }
+
+
+
+    @Test
+    public void createPokemonTest() {
+        Pokemon pokemon = new Pokemon(0, "Bulbasaur", 49, 49, 90, 110, 60, 400, 4, 56.0);
+        when(pokemonFactory.createPokemon(0, 110, 60, 400, 4)).thenReturn(pokemon);
+        assertEquals(pokemon, pokedex.createPokemon(0, 110, 60, 400, 4));
+    }
+
+    @Test
+    public void sizeTest() {
+        assertEquals(0, pokedex.size());
+        Pokemon pokemon = new Pokemon(0, "Bulbasaur", 49, 49, 90, 110, 60, 400, 4, 56.0);
+        when(pokemonFactory.createPokemon(1, 500, 600, 4500, 30)).thenReturn(pokemon);
+        pokedex.addPokemon(pokemon);
+        assertEquals(1, pokedex.size());
+    }
+
+    @Test
+    public void addAndGetPokemonTest() throws PokedexException {
+        Pokemon pokemon = new Pokemon(0, "Bulbasaur", 49, 49, 90, 110, 60, 400, 4, 56.0);
+        when(pokemonFactory.createPokemon(1, 500, 600, 4500, 30)).thenReturn(pokemon);
+        int id = pokedex.addPokemon(pokemon);
+        assertEquals(pokemon, pokedex.getPokemon(id));
+    }
+
+
+
+    @Test
+    public void getPokemonsTest() {
+        Pokemon pokemon1 = new Pokemon(0, "Bulbasaur", 49, 49, 90, 110, 60, 400, 4, 56.0);
+        Pokemon pokemon2 = new Pokemon(0, "Bulbasaur", 49, 49, 90, 110, 60, 400, 4, 56.0);
+        when(pokemonFactory.createPokemon(1, 500, 600, 4500, 30)).thenReturn(pokemon1);
+        when(pokemonFactory.createPokemon(2, 600, 700, 5500, 40)).thenReturn(pokemon2);
+        pokedex.addPokemon(pokemon1);
+        pokedex.addPokemon(pokemon2);
+
+        List<Pokemon> pokemons = pokedex.getPokemons();
+        assertEquals(2, pokemons.size());
+        assertTrue(pokemons.contains(pokemon1));
+        assertTrue(pokemons.contains(pokemon2));
+    }
+}
