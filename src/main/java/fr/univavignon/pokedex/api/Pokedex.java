3f038c2448a10018917d78df9b9ab8cf627a9167
Pierre-Cognard <pierre.cognard@alumni.univ-avignon.fr>
2023-04-20 09:39:13 +0200

    test ok normalement

--- src/main/java/fr/univavignon/pokedex/api/Pokedex.java
+++ src/main/java/fr/univavignon/pokedex/api/Pokedex.java
@@ -15,38 +15,45 @@ public class Pokedex implements IPokedex {
         this.pokemonFactory = pokemonFactory;
         this.pokemons = new ArrayList<>();
     }
-    @Override
-    public int size() {
-        return 0;
-    }
 
     @Override
-    public int addPokemon(Pokemon pokemon) {
-        return 0;
-    }
-
-    @Override
-    public Pokemon getPokemon(int id) throws PokedexException {
-        return null;
-    }
-
-    @Override
-    public List<Pokemon> getPokemons() {
-        return null;
-    }
-
-    @Override
-    public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
-        return null;
+    public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
+        return metadataProvider.getPokemonMetadata(index);
     }
 
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-        return null;
+        return pokemonFactory.createPokemon(index, cp, hp, dust, candy);
     }
 
     @Override
-    public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-        return null;
+    public int size() {
+        return pokemons.size();
+    }
+
+    @Override
+    public int addPokemon(Pokemon pokemon) {
+        pokemons.add(pokemon);
+        return pokemons.size() - 1;
+    }
+
+    @Override
+    public Pokemon getPokemon(int id) throws PokedexException {
+        if (id < 0 || id >= pokemons.size()) {
+            throw new PokedexException("Invalid Pokemon ID");
+        }
+        return pokemons.get(id);
+    }
+
+    @Override
+    public List<Pokemon> getPokemons() {
+        return pokemons;
+    }
+
+    @Override
+    public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
+        List<Pokemon> sortedPokemons = new ArrayList<>(pokemons);
+        sortedPokemons.sort(order);
+        return sortedPokemons;
     }
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -4,44 +4,25 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-
-import fr.univavignon.pokedex.api.IPokedexFactory;
-import fr.univavignon.pokedex.api.IPokemonMetadataProvider;
-import fr.univavignon.pokedex.api.IPokemonTrainerFactory;
-import fr.univavignon.pokedex.api.Pokemon;
-import fr.univavignon.pokedex.api.PokemonMetadata;
-import fr.univavignon.pokedex.api.PokemonTrainer;
-import fr.univavignon.pokedex.api.Team;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 public class IPokemonTrainerFactoryTest {
-    @Mock
-    private IPokedexFactory pokedexFactory;
-    @Mock
-    private IPokemonTrainerFactory pokemonTrainerFactory;
-    @Mock
-    private IPokemonMetadataProvider metadataProvider;
-    @Mock
-    private IPokemonFactory pokemonFactory;
-
-    @BeforeEach
+    @Mock private IPokedexFactory pokedexFactory;
+    @Mock private IPokemonTrainerFactory pokemonTrainerFactory;
+    @Mock private IPokemonMetadataProvider metadataProvider;
+    @Mock private IPokemonFactory pokemonFactory;
+    @Before
     public void init() throws PokedexException {
-        MockitoAnnotations.openMocks(this);
+        MockitoAnnotations.initMocks(this);
 
         when(pokemonFactory.createPokemon(0, 110, 60, 400, 4)).thenReturn(new Pokemon(0, "Bulbasaur", 49, 49, 90, 110, 60, 400, 4, 56.0));
-
         when(metadataProvider.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0, "Bulbasaur", 49, 49, 90));
-
         when(pokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(new Pokedex(metadataProvider, pokemonFactory));
-
         when(pokemonTrainerFactory.createTrainer("Ash Ketchum", Team.VALOR, pokedexFactory)).thenReturn(new PokemonTrainer("Ash Ketchum", Team.VALOR, pokedexFactory));
-
     }
 
     @Test
